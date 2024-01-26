ecf53409838ac628759e8d3c97b6c0ffaefc5d62
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-01-26 09:57:40 -0600

    created Add Items view

---
+++ To-Do List/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  To-Do List
+//
+//  Created by Fabiola Rocha Marquez on 1/26/24.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
